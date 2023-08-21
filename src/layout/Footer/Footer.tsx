f419e4db983868efbae60a726ca33df71ae1dc20
Kicka3 <kirill_ozornin@mail.ru>
2023-08-21 20:11:37 +0500

    Add iconLists

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,19 +4,23 @@ import {FlexWrapper} from "../../components/FlexWrapper";
 import {WhatsUpIconFooter} from "../../components/svgIcons/icons/WhatsUpIconFooter";
 import {TelegramIconFooter} from "../../components/svgIcons/icons/TelegramIconFooter";
 import {LinkedInIconFooter} from "../../components/svgIcons/icons/LinkedInIconFooter";
+import {FooterIcon} from "../../components/svgIcons/icons/FooterIcon";
 
 export const Footer = () => {
     return (
         <StyledFooter>
             <FlexWrapper marginB={"60px"}>
-                <WhatsUpIconFooter/>
-                <TelegramIconFooter/>
-                <LinkedInIconFooter/>
+                <SocialIconLink>
+                    <WhatsUpIconFooter/>
+                    <TelegramIconFooter/>
+                    <LinkedInIconFooter/>
+                </SocialIconLink>
             </FlexWrapper>
+            <FooterIcon/>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-  
+
 `;
