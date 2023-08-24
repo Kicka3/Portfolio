4dcdfe508d5edd10ec8b9ab0d19e398142f49168
Kirill <kirill_ozornin@mail.ru>
2023-08-24 21:09:35 +0200

    Refactoring ACBLT

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,19 +4,21 @@ import {FlexWrapper} from "../../components/FlexWrapper";
 import {WhatsUpIconFooter} from "../../components/svgIcons/icons/WhatsUpIconFooter";
 import {TelegramIconFooter} from "../../components/svgIcons/icons/TelegramIconFooter";
 import {LinkedInIconFooter} from "../../components/svgIcons/icons/LinkedInIconFooter";
-import {FooterIcon} from "../../components/svgIcons/icons/FooterIcon";
+import {FooterImg} from "../../components/svgIcons/icons/FooterIcon";
+
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <FlexWrapper marginB={"60px"}>
+            <FlexWrapper marginB={"60px"} direction={"column"}>
                 <SocialIconLink>
                     <WhatsUpIconFooter/>
                     <TelegramIconFooter/>
                     <LinkedInIconFooter/>
                 </SocialIconLink>
             </FlexWrapper>
-            <FooterIcon/>
+            <FooterImg/>
+            <StyledSmall>Coded by Oz Kirill</StyledSmall>
         </StyledFooter>
     );
 };
@@ -30,4 +32,12 @@ const SocialIconLink = styled.a`
   display: flex;
   gap: 20px;
   margin-bottom: 200px;
+`;
+
+const StyledSmall = styled.small`
+  color: #eb0f0f;
+  font-family: Inter;
+  font-size: 20px;
+  align-items: center;
+  top: 100px;
 `;
