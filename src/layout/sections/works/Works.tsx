89b989241a6cddc4621c4bc19590c8763b6d0f92
Kirill <kirill_ozornin@mail.ru>
2023-09-02 15:38:19 +0200

    Refactoring Main

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -4,15 +4,21 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Subtitle} from "../../../components/Subtitle";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
-import work1 from "../../../assets/images/Work1.png";
-import work2 from "../../../assets/images/Work2.png";
-import work3 from "../../../assets/images/Work3.png";
-import work4 from "../../../assets/images/Work4.png";
-import work5 from "../../../assets/images/Rectangle3.png"
 import {CircleIcon} from "../../../components/svgIcons/icons/CircleIcon";
 import {Container} from "../../../components/Container";
 import {BgDotsWork} from "../../../components/BgDotsWork";
 import {BgXWork} from "../../../components/BgXWork";
+
+import work1 from "../../../assets/images/Work1.png";
+import work2 from "../../../assets/images/Work2.png";
+import work3 from "../../../assets/images/Work3.png";
+import work4 from "../../../assets/images/Work4.png";
+import work5 from "../../../assets/images/Work5.png";
+import work6 from "../../../assets/images/Work6.png";
+import work7 from "../../../assets/images/Work7.png";
+import work8 from "../../../assets/images/Work8.png";
+
+
 
 
 // const worksItems = ["All", "Landing page", "react", "SPA",];
@@ -37,9 +43,13 @@ export const Works = () => {
                               text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
                         <Work src={work4} title={"Project Tile goes here"}
                               text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
-                        <Work src={work1} title={"Project Tile goes here"}
-                              text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
                         <Work src={work5} title={"Project Tile goes here"}
+                              text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
+                        <Work src={work6} title={"Project Tile goes here"}
+                              text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
+                        <Work src={work7} title={"Project Tile goes here"}
+                              text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
+                        <Work src={work8} title={"Project Tile goes here"}
                               text={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"}/>
                     </FlexWrapper>
 
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -6,8 +6,8 @@ import work3 from "../../../../assets/images/Rectangle2.png"
 
 
 type WorkPropsType = {
-    title: string,
-    text: string,
+    title?: string,
+    text?: string,
     src: string,
 
 }
@@ -15,7 +15,7 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
 
-        <StyledWork>
+        <StyledWork src={props.src}>
 
             <WorkCardsWrapper>
                 <Title>{props.title}</Title>
@@ -53,7 +53,8 @@ const WorkCardsWrapper = styled.div`
   border-radius: 0 0 26px 26px;
 `;
 
-const StyledWork = styled.div`
+
+const StyledWork = styled.div<WorkPropsType>`
   box-shadow: 2px 2px 100px 0 rgba(0, 0, 0, 0.20);
   max-width: 270px;
   width: 100%;
@@ -62,7 +63,8 @@ const StyledWork = styled.div`
   border-radius: 26px 26px;
   background-color: #F9F9FC;
   
-  background-image: url(${work3});
+  //background-image: url(${work3});
+  background-image: url(${props => props.src});
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
@@ -71,10 +73,10 @@ const StyledWork = styled.div`
 `;
 
 const Title = styled.h3`
-  padding-left: 33px;
+  //padding-left: 33px;
   padding-right: 33px;
   margin-bottom: 29px;
-  text-align: left;
+  //text-align: left;
   font-size: 20px;
   color: #2157F2;
   overflow: hidden;
