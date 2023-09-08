c7112162a3e8d43ec9e3ec9f31ead77897c07d72
Kirill <kirill_ozornin@mail.ru>
2023-09-08 22:47:34 +0200

    Add gotop4

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import React from "react";
 function App() {
     return (
         <div className="App">
-            {/*<Partical/>*/}
+            <Partical/>
             <Header/>
             <MobileHeaderMenu/>
             <Main/>
@@ -37,8 +37,8 @@ function App() {
 
 export default App;
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #e91e63;
-`;
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: #e91e63;
+// `;
--- src/components/goTop/GoTop.tsx
+++ src/components/goTop/GoTop.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {Icon} from "../icon/icon";
 import {theme} from "../../styles/Theme";
 import {animateScroll as scroll} from "react-scroll";
 
 export const GoTop = () => {
-   return (
-      <StyledGoTop>
-            <Icon iconId={"arrowR"} height={"30px"} width={"30px"} viewBox={"0 0 30 30"} color={theme.whiteThemeColors.goTopBtn}/>
-      </StyledGoTop>
-   );
+
+    const [showBtn, setshowBtn] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200) {
+                setshowBtn(true)
+            } else {
+                setshowBtn(false)
+            }
+        })
+    }, []);
+
+    return (
+        <>
+            {showBtn && (
+                <StyledGoTop onClick={() => {
+                    scroll.scrollToTop()
+                }}>
+                    <Icon iconId={"arrowR"} height={"30px"} width={"30px"} viewBox={"0 0 30 30"}
+                          color={theme.whiteThemeColors.goTopBtn}/>
+                </StyledGoTop>
+            )}
+        </>
+    );
 };
 
 const StyledGoTop = styled.button`
-   background-color: transparent;
-   transform: rotate(-90deg);
-   text-align: center;
-   border: none;
-   cursor: pointer;
-   padding: 8px;
-   position: fixed;
-   bottom: 280px;
-   right: 30px;
-   transition: all 0.2s ease 0s;
-   
-   &:hover {
-      transform: rotate(-90deg) scale(1.2)
-   }
+  background-color: transparent;
+  transform: rotate(-90deg);
+  text-align: center;
+  border: none;
+  cursor: pointer;
+  padding: 8px;
+  position: fixed;
+  bottom: 280px;
+  right: 30px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    transform: rotate(-90deg) scale(1.2)
+  }
 `;
